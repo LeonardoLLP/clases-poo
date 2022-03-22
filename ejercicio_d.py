696f2e2f2d51ad5863d6002e20ff4a172aaeabe3
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-03-22 14:55:09 +0100

    init and del functions

--- ejercicio_d.py
+++ ejercicio_d.py
@@ -1,5 +1,12 @@
-class Test()
+class Test():
+    def __init__(self):
+        self.logs = 0
+        self.archive = open("logs.txt", "r")
+        self.archive.write("--Start log--")
 
+
+    def __del__(self):
+        self.archive.write("--End log: {} log(s)--".format(self.logs))
 
 test = Test() 
 for i in range(1, 6): 
