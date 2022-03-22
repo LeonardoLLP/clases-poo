3defddceede8ac3fcd42699d6d8f3da530aef3cf
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-03-22 14:19:24 +0100

    Establecidos returns ejercicio 1

--- ejercicio_a.py
+++ ejercicio_a.py
@@ -1,11 +1,16 @@
 class Palindromo():
     def esPalindromo(text: str):
         back_text = text[::-1]
-        # print(text)
-        # print(back_text)
-        if text == back_text
+        if text.casefold() == back_text.casefold():
+            return True
+        else:
+            return False
 
 
 
 
 print(Palindromo.esPalindromo('radar'))
+print(Palindromo.esPalindromo('sonar'))
+print(Palindromo.esPalindromo('Arde ya la yedra'))
+print(Palindromo.esPalindromo('Ardeyalayedra'))
+print(Palindromo.esPalindromo('!@#$% %$#@!'))
