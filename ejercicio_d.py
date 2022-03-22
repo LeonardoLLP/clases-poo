3babc637933c6678362323c415ddbd03a458628f
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-03-22 15:02:01 +0100

    SOy estupido y estaba abriendo el archivo en write mode

--- ejercicio_d.py
+++ ejercicio_d.py
@@ -1,12 +1,16 @@
 class Test():
     def __init__(self):
         self.logs = 0
-        self.archive = open("logs.txt", "r")
-        self.archive.write("--Start log--")
+        self.file = open("logs.txt", "w")
+        self.file.write("--Start log--")
 
+    def llamada(self, text):
+        self.file.write(text)
+        self.logs += 1
 
     def __del__(self):
-        self.archive.write("--End log: {} log(s)--".format(self.logs))
+        self.file.write("--End log: {} log(s)--".format(self.logs))
+        self.file.close()
 
 test = Test() 
 for i in range(1, 6): 
