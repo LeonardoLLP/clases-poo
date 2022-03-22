ed700255109bccc5bf440f291c45ff3020f81b18
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-03-22 14:15:29 +0100

    Comprobacion palindromo

---
+++ ejercicio_a.py
@@ -0,0 +1,11 @@
+class Palindromo():
+    def esPalindromo(text: str):
+        back_text = text[::-1]
+        # print(text)
+        # print(back_text)
+        if text == back_text
+
+
+
+
+print(Palindromo.esPalindromo('radar'))
