98b892c44d5723ddc3262bb5eb2339ad40071aad
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-03-22 14:49:06 +0100

    Acabado ejercicio c

--- ejercicio_c.py
+++ ejercicio_c.py
@@ -21,3 +21,32 @@ print(aa.length((value,1,'value')))
 
 #! Reescribimos código equivalente para hacerlo más comprensible
 
+class A:
+    def value(self):
+        return self
+
+    def length(self, t):
+        return len(t)
+
+a = A
+
+print(a.value(a))
+# Prints the class name and the memory location
+
+instance = a()
+print(instance is a())
+### Instances memory vary each time they are created. So instance memory is not the same as a() alocation.
+
+print(A.length(()))  # Zero elements tuple, length = 0
+print(A.length((a,)))  # One element tuple, length = 1
+print(A.length((instance, (a, a.length))))  # Two element tuple (careful with tuple inside the tuple: counts as only one element)
+print(A.length(((aa.length, 1, "value"))))  # Three string tuple: length = 3
+
+#? In conclusion:
+
+#? >>> <class>
+#? False
+#? 0
+#? 1
+#? 2
+#? 3
